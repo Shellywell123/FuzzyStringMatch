42c200b885203f025502750cb4e21b606c44cfcb
Shellywell123 <benjamin@shellswell.com>
2020-08-27 00:11:14 +0100

    written some functions

--- RESOURCES.md
+++ RESOURCES.md
@@ -1,1 +1,1 @@
-[first link found by Fergus]{https://www.datacamp.com/community/tutorials/fuzzy-string-python}
+{first link found by Fergus}[https://www.datacamp.com/community/tutorials/fuzzy-string-python]
--- main.py
+++ main.py
@@ -1,21 +1,48 @@
+import Levenshtein as lev #to be replaced by fuzzy
+
+########################################################################################
+
+# to be made into a class
+
+def get_ratio_and_dist(input_string,correct_string):
+    """
+    returns the lev ratio, lev dist between two strings
+    """
+    ratio =lev.ratio(input_string,correct_string)
+    distance=lev.distance(input_string,correct_string)
+
+    return ratio,distance
 
 
-correct_string = 'Correct String!'
-input_strings = ['correct_string','correct string','correct String','Correct string']
+def auto_correct(input_string,correct_string):
+    """
+    will attempt to find a match for a string
+    INCOMPLETE
+    """
+    ratio,original_distance = get_ratio_and_dist(input_string,correct_string)
+    distance = original_distance
+    print(ratio,distance)
 
-for input_string in input_strings:
-    result = input_string == correct_string
-
-    if result == False:
+    if ratio < 1:
         # not inital match
-        result = input_string.lower() == correct_string.lower()
 
-        if result == False:
-            #not a case problem
+        while distance != 0:
+            input_string,correct_string = input_string.lower(),correct_string.lower()
+            ratio,distance = get_ratio_and_dist(input_string,correct_string)
+
             print('no match found for: "{}"'.format(input_string))
 
         else:
-            print('match found!')
-
+            print('match found from {} fixes'.format(original_distance))
     else:
-        print('match found!')
+        print('initial match found!')
+
+########################################################################################
+
+correct_string = 'Correct String!'
+input_strings = ['correct string','correct String','Correct string','Correct String!']
+
+for input_string in input_strings:
+    auto_correct(input_string,correct_string)
+    
+
